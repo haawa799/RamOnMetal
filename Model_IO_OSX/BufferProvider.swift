0004381f1f41610bc725f62a970fa4c9b97e201f
Andrii Kharchyshyn <Andrii.Kharchyshyn@refinitiv.com>
2021-12-01 14:47:23 +0200

    Update for Xcode 13

--- Model_IO_OSX/BufferProvider.swift
+++ Model_IO_OSX/BufferProvider.swift
@@ -78,10 +78,16 @@ class BufferProvider: NSObject {
 extension GLKMatrix4 {
     
     var raw: [Float] {
-        var tmp = self.m
-        let array = [Float](UnsafeBufferPointer(start: &tmp.0,
-                                                count: MemoryLayout.size(ofValue: tmp)/MemoryLayout<Float>.size))
-        return array
+//        var tmp = self.m
+//
+//        let q = tmp.0
+//
+//        let pointer = UnsafeBufferPointer(start: &tmp.0,
+//                                          count: MemoryLayout.size(ofValue: tmp)/MemoryLayout<Float>.size)
+//
+//
+//        let array = [Float](pointer)
+        return [m.0, m.1, m.2, m.3, m.4, m.5, m.6, m.7, m.8, m.9, m.10, m.11, m.12, m.13, m.14, m.15]
     }
     
 }
